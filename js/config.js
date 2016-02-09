3d00af3f526c95ee5d3e0623b22ecb9f802a299f
Wasin Thonkaew <haxpor@gmail.com>
2016-02-09 14:55:25 +0700

    Add a new line style

--- js/config.js
+++ js/config.js
@@ -34,6 +34,13 @@ var jsonConfig = {
 			"stroke_width": 1,
 			"stroke_dasharray": null,
 			"stroke_linecap": "round"
+		},
+        {
+			"fill": "none",
+			"stroke": "rgb(128,192,44)",
+			"stroke_width": 2,
+			"stroke_dasharray": "5,5",
+			"stroke_linecap": "round"
 		}
 	]
 };
